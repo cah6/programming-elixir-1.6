32eb1003235d31b6f28c522dfa77511a444ea739
Christian Henry <christian.henry92@gmail.com>
2021-02-12 09:52:33 -0500

    implement Strings.calculator

--- lib/11-strings.exs
+++ lib/11-strings.exs
@@ -2,4 +2,20 @@ defmodule Strings do
   def is_ascii(str) do
     Enum.all?(Enum.map(str, &(&1 >= ?\s && &1 <= ?~)))
   end
+
+  def calculator(str) do
+    splitf = fn x -> Enum.split_while(x, &(&1 != 32)) end
+    {s1, rest} = splitf.(str)
+    {op, rest} = splitf.(Enum.drop(rest, 1))
+    s2 = Enum.drop(rest, 1)
+    x = List.to_integer(s1, 10)
+    y = List.to_integer(s2, 10)
+
+    case op do
+      '+' -> x + y
+      '-' -> x - y
+      '/' -> x / y
+      '*' -> x * y
+    end
+  end
 end

fe52a71a98681950c0c46d6b8879322ea2220f79
Christian Henry <christian.henry92@gmail.com>
2021-02-14 19:26:11 -0500

    Write empty deps section, pin mix.lock

--- mix.exs
+++ mix.exs
@@ -4,7 +4,12 @@ defmodule MyApp.MixProject do
   def project do
     [
       app: :my_app,
-      version: "1.0.0"
+      version: "1.0.0",
+      deps: deps()
     ]
+  end
+
+  defp deps do
+    []
   end
 end
